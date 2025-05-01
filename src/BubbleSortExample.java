e1afec2e319f5c6c1b4424cb82b777486d8b84a3
Tim Gassan <tim-gassan@t-online.de>
2025-05-01 09:58:28 +0200

    optimized BubbleSort with 'break'

--- src/BubbleSortExample.java
+++ src/BubbleSortExample.java
@@ -3,14 +3,17 @@ public class BubbleSortExample {
         int n = arr.length;
         int temp = 0;
             for(int i=0; i < n; i++) {
+                boolean swapped = false;
                 for(int j=1; j < (n-i); j++) {
                     if(arr[j-1] > arr[j]) {
                         //swap elements
                         temp = arr[j-1];
                         arr[j-1] = arr[j];
                         arr[j] = temp;
+                        swapped = true;
                     }
                 }
+                if (swapped = false) break;
             }
     }
 }
